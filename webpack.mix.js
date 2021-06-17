d49fdf5adfc20d5176044e6483f20114a334f0d6
parables <parables95@gmail.com>
2021-06-17 10:09:52 +0000

    Before npm prune

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,6 @@
 const mix = require("laravel-mix");
 require("laravel-mix-svelte");
-require("laravel-mix-tailwind");
-// require('laravel-mix-auto-extract');
-// require("laravel-mix-artisan-serve");
-const tailwindJit = require("@tailwindcss/jit");
+require("laravel-mix-artisan-serve");
 
 
 /*
@@ -19,7 +16,7 @@ const tailwindJit = require("@tailwindcss/jit");
 
 mix.js("resources/js/app.js", "public/js")
     .extract(["svelte"])
-    .tailwind()
+    // .tailwind()
     .svelte({
         dev: !mix.inProduction(),
     })
@@ -28,11 +25,13 @@ mix.js("resources/js/app.js", "public/js")
             chunkFilename: "js/[name].js?id=[chunkhash]",
         },
     })
-    .sass("resources/css/app.scss", "public/css/app.css") // for sass
+    .sass("resources/css/app.scss", "public/css/app.css")
     .options({
         postCss: [
-            tailwindJit,
+            require("@tailwindcss/jit"),
+            require("postcss-import"),
             require("css-declaration-sorter")({ order: "smacss" }), // a personal preference, can be removed
+
         ],
         autoprefixer: {
             // this entire object can be empty, i believe laravel mix handles has a lower version setting
@@ -41,8 +40,8 @@ mix.js("resources/js/app.js", "public/js")
             },
         },
     })
-    .browserSync('localhost:8000');
-    // .serve();
+    .browserSync('localhost:8000')
+    .serve();
 
 // only cache version in prod
 if (mix.inProduction()) {
