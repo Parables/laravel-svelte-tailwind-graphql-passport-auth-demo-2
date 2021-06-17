c740d90ea77179c721faef366cbfa7111d447479
parables <parables95@gmail.com>
2021-06-17 10:46:43 +0000

    final version

--- resources/js/Pages/Index.svelte
+++ resources/js/Pages/Index.svelte
@@ -2,9 +2,9 @@
     let name = "world";
 </script>
 
+<h1 class="text-green-600 bg-teal">Hello {name}, welcome to Svelte</h1>
+<input bind:value={name} />
+<button class="btn-blue " type="submit">Submit</button>
+
 <style>
 </style>
-
-<h1 class="text-yellow-600 bg-teal">Hello {name}, welcome to Svelte</h1>
-<input bind:value="{name}"/>
-<button class="btn-blue" type="submit">Submit</button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  purge: [
-    './storage/framework/views/*.php',
-    './resources/**/*.blade.php',
-    './resources/**/*.js',
-    './resources/**/*.vue',
-    './resources/**/*.svelte',
-
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-        colors:{
-            primary: "#D2F3F3",
-            teal: "#F3A3FF"
+    mode: "jit",
+    purge: [
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./resources/**/*.svelte",
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                primary: "#D2F3F3",
+                teal: "#F3A3FF",
+            },
+            fontFamily: {
+                display: ["Oduda"],
+            },
         },
-        fontFamily: {
-            display: ['Oduda']
-        }
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
